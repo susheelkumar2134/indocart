2be754e6eb00b52145afdd8bbc2a778a00b66053
susheel <sumitkumar262001@gmail.com>
2023-10-31 10:59:58 +0530

    try

--- indoproject/settings.py
+++ indoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_691vn90_^751s)d53ewqm$5%9dhx4)h(#b_8qkl0!apy#8+5e
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app','now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
